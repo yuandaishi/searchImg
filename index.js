dfb65eb8b20d998dfdbc15af99d09e69d0de5b49
yuandaishi <623926557@qq.com>
2021-05-03 23:05:31 +0800

    添加await

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 // http://www.44932.com
-//https://www.jpxgmn.com/
+//https://www.jpxgmn.com/ https://p.plmn5.com
+//https://www.xrmn.cc/ https://pic.xrmn.cc
 var model = require("./model"),
-    basicPath = "https://www.jpxgmn.com/Xgyw/Xgyw5846.html",//协议如果是https，则不能用http代替
-    baseImg = 'https://p.plmn5.com',//图片地址前缀
+    basicPath = "https://www.xrmn.cc/azhu.html",//协议如果是https，则不能用http代替
+    // baseImg = 'https://p.plmn5.com',//图片地址前缀 https://www.jpxgmn.com/
+    baseImg = 'https://pic.xrmn.cc',//图片地址前缀 https://www.xrmn.cc/
+    webUrl = 'https://www.xrmn.cc',//网页地址url
     imageNum,//页码
     startNum = 0, //图片名称
     downloadPath = '';//文件夹名称
 
-const init = async url => {//初始化，获取套图页码，创建套图文件夹
-    const data = await model.getPage(url);//获取网页内容
-    downloadPath = model.setFileName(data, '.article-title');//定义文件夹名称
-    imageNum = model.getImagesNum(data, '.pagination a');//获取这组图片的页码
-    model.mkdirSync(downloadPath);//创建文件夹
-    for (let i = 0; i < imageNum; i++) {//翻页处理
-        let url;
-        if (i === 0) {
-            url = basicPath;
-        } else {
-            url = basicPath.substring(0, basicPath.length - 5) + `_${i}.html`
+const init = async basicPath => {//初始化，获取套图页码，创建套图文件夹
+    const dataMain = await model.getPage(basicPath);//获取网页内容
+    let list = await getList(dataMain, webUrl);
+    // console.log(list);
+    // return
+    for (let i = 58; i < list.length; i++) {
+        //console.log(list[i].url)
+        const data = await model.getPage(list[i]);//获取网页内容
+        // downloadPath = model.setFileName(data, '.article-title');//定义文件夹名称 https://www.jpxgmn.com/
+        // imageNum = model.getImagesNum(data, '.pagination a');//获取这组图片的页码 https://www.jpxgmn.com/
+        downloadPath = model.setFileName(data, 'h1');
+        imageNum = model.getImagesNum(data, '.page a');//获取这组图片的页码
+        //console.log(imageNum)
+        model.mkdirSync(downloadPath);//创建文件夹(同步)
+        for (let j = 0; j < imageNum; j++) {//翻页处理
+            let url;
+            if (j === 0) {
+                url = list[i];
+            } else {
+                url = list[i].substring(0, list[i].length - 5) + `_${j}.html`
+            }
+            await main(url);
         }
-        main(url);
     }
+    // console.log(url)
+    // const data = await model.getPage(basicPath);//获取网页内容
+    // // downloadPath = model.setFileName(data, '.article-title');//定义文件夹名称 https://www.jpxgmn.com/
+    // // imageNum = model.getImagesNum(data, '.pagination a');//获取这组图片的页码 https://www.jpxgmn.com/
+    // downloadPath = model.setFileName(data, 'h1');
+    // imageNum = model.getImagesNum(data, '.page a');//获取这组图片的页码
+    // // console.log(imageNum)
+    // model.mkdirSync(downloadPath);//创建文件夹
+    // for (let j = 0; j < imageNum; j++) {//翻页处理
+    //     let url = basicPath;
+    //     if (j === 0) {
+    //         url = url;
+    //     } else {
+    //         url = url.substring(0, url.length - 5) + `_${j}.html`
+    //     }
+    //     main(url);
+    // }
+}
+
+
+const getList = async (data, url) => {//获取人物套图list
+    let list = await model.getModelList(data, '.update_area_lists .i_list a', url);
+    return list;
 }
 
 const main = async url => {
     const data = await model.getPage(url);
     let list = model.getImgObj(data, 'p img', baseImg);//套图组的信息（名称和url
-    await model.downloadImage(list, downloadPath, startNum);//下载图片
-    startNum += list.length;
+    await model.downloadImage(list, downloadPath);//下载图片
+    //startNum += list.length;
 };
 init(basicPath);
--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 const rq = require("request-promise"); //进入request-promise模块，需要安装request模块
 const fs = require('fs');
 const cheerio = require('cheerio');
-const depositPath = 'D:/WeChat/locales/';//存放照片的位置(要先创建这个文件夹)
+const depositPath = 'D:/吉他相关/新建文件夹/你还要我怎样/图片/阿朱/';//存放照片的位置(要先创建这个文件夹)
 module.exports = {
     getPage: async (url) => {
         const data = {
@@ -18,7 +18,22 @@ module.exports = {
     },
     getImagesNum: (data, farNode) => {//根据需要获取的套图, 获取页码
         const $ = cheerio.load(data.res); //将html转换为可操作的节点
-        return $(farNode).length / 2 - 1;
+        // return $(farNode).length / 2 - 1;
+        return $(farNode).length - 1;
+    },
+    getModelList: (data, farNode, webUrl) => {//获取模特套图url集合
+        let list = [];
+        const $ = cheerio.load(data.res); //将html转换为可操作的节点
+        $(farNode).each(async (i, e) => {
+            // let obj = {//不同的页面，写法不同
+            //     name: e.attribs.alt, //图片网页的名字，后面作为文件夹名字(根据实际页面)
+            //     url: webUrl + e.attribs.href//图片网页的url
+            // }
+            let url = webUrl + e.attribs.href//图片网页的url
+            //console.log(obj)
+            list.push(url); //输出目录页查询出来的所有链接地址
+        })
+        return list;
     },
     getImgObj: (data, farNode, baseImg) => {//获取每页套图的信息
         let list = [];//需要获取的套图名称和url信息
@@ -34,16 +49,16 @@ module.exports = {
         return list;
     },
     mkdirSync: (name) => {//根据需要获取的套图，创建文件夹
-        var downloadPath = depositPath + name.replace(/[!@#%&\/<>]/gi, '-');//名称有/的话，则会有路径问题，要把/转化为-
+        var downloadPath = depositPath + name.replace(/[\!\#%\/<>]/gi, '-');//名称有/的话，则会有路径问题，要把/转化为-
         if (!fs.existsSync(downloadPath)) {//查看是否存在这个文件夹
             fs.mkdirSync(downloadPath);//不存在就建文件夹
-            console.log(`${name}文件夹创建成功`);
+            console.log(`文件夹创建成功`);
         } else {
-            console.log(`${name}文件夹已经存在`);
+            console.log(`文件夹已经存在`);
         }
     },
     //下载相册照片
-    downloadImage: async (list, downloadPath, startNum) => {
+    downloadImage: async (list, downloadPath) => {
         // if (data.res) {
         //     var $ = cheerio.load(data.res);
         //     if ($(imgNode).find("img")) {
@@ -79,8 +94,14 @@ module.exports = {
             await rq({
                 url: list[i].url,//图片地址
                 resolveWithFullResponse: true,
-            }).pipe(fs.createWriteStream(`${depositPath}${downloadPath}/${name}`));//下载
-            console.log(`${downloadPath}/${name}下载成功`);
+            }).pipe(fs.createWriteStream(`${depositPath}${downloadPath}/${name}`), { autoClose: true });//下载
+            console.log(`${name}下载成功`);
+            // try {
+            //     res.pipe(fs.createWriteStream(`${depositPath}${downloadPath}/${name}`, { autoClose: true }));//下载
+            //     console.log(`${downloadPath}/${name}下载成功`);
+            // } catch (error) {
+            //     console.log(error)
+            // }
         }
     }
 }
